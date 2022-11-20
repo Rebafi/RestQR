53f8aabab827184c5fef5f6875708d9a8895f225
molchanova taisiya <101670269+THAIPHOO@users.noreply.github.com>
2022-11-20 12:58:47 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/restqr/Helper/ManagementCart.java
@@ -0,0 +1,68 @@
+package com.example.restqr.Helper;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.restqr.Domain.FoodDomain;
+import com.example.restqr.Interface.ChangeQuantityListener;
+
+import java.util.ArrayList;
+
+public class ManagementCart {
+    private Context context;
+    private TinyDB tinyDB;
+
+    public ManagementCart(Context context){
+        this.context = context;
+        this.tinyDB = new TinyDB(context);
+    }
+
+    public void insertFood(FoodDomain item){
+        ArrayList<FoodDomain> shoppingList = getListCart();
+        boolean existAlready = false;
+        int n = 0;
+        for(int i = 0; i < shoppingList.size(); i++){
+            if(shoppingList.get(i).getTitle().equals(item.getTitle())){
+                existAlready = true;
+                n = i;
+                break;
+            }
+        }
+        if(existAlready){
+            shoppingList.get(n).setQuantity(item.getQuantity());
+        } else {
+            shoppingList.add(item);
+        }
+        tinyDB.putListObject("CardList", shoppingList);
+        Toast.makeText(context, "added to your cart", Toast.LENGTH_SHORT).show();
+    }
+
+    public ArrayList<FoodDomain> getListCart(){
+        return tinyDB.getListObject("CartList");
+    }
+
+    public void incrementQuantity(ArrayList<FoodDomain> shoppingList, int position, ChangeQuantityListener changeQuantityListener){
+        shoppingList.get(position).setQuantity(shoppingList.get(position).getQuantity() + 1);
+        tinyDB.putListObject("CartList", shoppingList);
+        changeQuantityListener.changed();
+    }
+
+    public void decrementQuantity(ArrayList<FoodDomain> shoppingList, int position, ChangeQuantityListener changeQuantityListener){
+        if(shoppingList.get(position).getQuantity()==1){
+            shoppingList.remove(position);
+        } else {
+            shoppingList.get(position).setQuantity(shoppingList.get(position).getQuantity() - 1);
+        }
+        tinyDB.putListObject("CartList", shoppingList);
+        changeQuantityListener.changed();
+    }
+
+    public Double getTotalPrice(){
+        ArrayList<FoodDomain> shoppingList = getListCart();
+        double fee = 0;
+        for(int i = 0; i < shoppingList.size(); i++){
+            fee += (shoppingList.get(i).getPrice() * shoppingList.get(i).getQuantity());
+        }
+        return fee;
+    }
+}
